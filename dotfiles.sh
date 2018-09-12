536b3ccf05726fab582bf2c27f8ec2ce4953e8b2
Mikhail Kilianovski <kilianovski@gmail.com>
2018-09-12 14:23:09 +0300

    Symlink gitconfig during the installation

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
+DOTFILES_ROOT=$(pwd -P)
+
 . bin/helpers.sh
+
+function link_file () {
+    rm $2
+    ln -s $1 $2
+}
 
 function install_step () {
     cd $1    
     . "install.sh"
+
+
+    for src in $(find "$DOTFILES_ROOT/$1" -name '*.symlink')
+    do
+        dst="$HOME/$(basename "${src%.*}")"
+        link_file "$src" "$dst"
+    done
+
+
     cd ..
 }
 
 install_step ubuntu
+install_step git
 install_step vscode
